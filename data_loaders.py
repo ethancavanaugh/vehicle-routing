8d8d9499483d458b18eec9745ed7a439d90e542f
ethancavanaugh <ethancavanaugh@icloud.com>
2024-09-17 13:51:36 -0400

    Extracts data loading functions to a separate module

---
+++ data_loaders.py
@@ -0,0 +1,63 @@
+import csv
+from OpenAddressHashTable import OpenAddressHashTable
+from Package import Package
+
+
+# Returns a 2d array with the distance between every address
+# Address indexes in this map are retrieved from the addr_index_map
+def load_distance_data(dist_csv: str) -> list:
+    print("Loading distance data...")
+    distance_table = []
+
+    with open(dist_csv, "r", encoding='utf-8-sig') as distance_file:
+        dist_reader = csv.reader(distance_file, delimiter=',')
+        for reader_row in dist_reader:
+            row = []
+            for dist in reader_row:
+                row.append(float(dist)) if dist != '' else float('inf')
+            distance_table.append(row)
+
+    # Fill in the rest of the table, mirroring the distances since the csv file only shows one direction
+    # i.e. distance from b->a = a->b
+    for i in range(len(distance_table)):
+        for j in range(i + 1, len(distance_table)):
+            distance_table[i].append(distance_table[j][i])
+
+    return distance_table
+
+
+# Returns a map of package id -> package object
+def load_package_data(pkg_csv: str) -> OpenAddressHashTable:
+    print("Loading package data...")
+    pkg_map = OpenAddressHashTable()
+
+    with open(pkg_csv, encoding='utf-8-sig') as package_file:
+        package_reader = csv.reader(package_file)
+        next(package_reader)  # Skip header
+        for row in package_reader:
+            id = int(row[0])
+            address = row[1]
+            city = row[2]
+            state = row[3]
+            zip_code = row[4]
+            deadline = row[5]
+            weight = int(row[6])
+            notes = row[7]
+
+            package = Package(id, address, city, state, zip_code, deadline, weight, notes)
+            pkg_map[id] = package
+
+    return pkg_map
+
+
+# Returns a map of addresses to their index in the distance table
+def load_address_data(addr_csv: str) -> dict:
+    print("Loading address data...")
+
+    addr_map = {}
+    with open(addr_csv, encoding='utf-8-sig') as address_file:
+        addr_reader = csv.reader(address_file)
+        for row in addr_reader:
+            addr_map[row[1]] = addr_reader.line_num - 1
+
+    return addr_map
--- main.py
+++ main.py
@@ -1,64 +1,13 @@
 # Ethan Cavanaugh
 # Student ID: 010962337
 
-from OpenAddressHashTable import OpenAddressHashTable
-from Package import Package
-import csv
+import data_loaders
 
-PACKAGE_CSV = "WGUPS Package File.csv"
-ADDRESS_CSV = "WGUPS Address Table.csv"
-DISTANCE_CSV = "WGUPS Distance Table.csv"
 HUB_ADDRESS = "4001 South 700 East"
-
-def load_package_data():
-    print("Loading package data...")
-
-    with open(PACKAGE_CSV, encoding='utf-8-sig') as package_file:
-        package_reader = csv.reader(package_file)
-        next(package_reader)  # Skip header
-        for row in package_reader:
-            id = int(row[0])
-            address = row[1]
-            city = row[2]
-            state = row[3]
-            zip_code = row[4]
-            deadline = row[5]
-            weight = int(row[6])
-            notes = row[7]
-
-            package = Package(id, address, city, state, zip_code, deadline, weight, notes)
-            package_map[id] = package
-
-
-def load_address_data():
-    print("Loading address data...")
-
-    with open(ADDRESS_CSV, encoding='utf-8-sig') as address_file:
-        addr_reader = csv.reader(address_file)
-        for row in addr_reader:
-            address_index_map[row[1]] = addr_reader.line_num - 1
-
-
-def load_distance_data():
-    print("Loading distance data...")
-
-    with open(DISTANCE_CSV, "r", encoding='utf-8-sig') as distance_file:
-        dist_reader = csv.reader(distance_file, delimiter=',')
-        for reader_row in dist_reader:
-            row = []
-            for dist in reader_row:
-                row.append(float(dist)) if dist != '' else float('inf')
-            distance_table.append(row)
-
-    # Fill in the rest of the table, mirroring the distances since the csv file only shows one direction
-    # i.e. distance from b->a = a->b
-    for i in range(len(distance_table)):
-        for j in range(i + 1, len(distance_table)):
-            distance_table[i].append(distance_table[j][i])
 
 
 def distance_between(addr1, addr2):
-    return distance_table[address_index_map[addr1]][address_index_map[addr2]]
+    return distance_table[addr_index_map[addr1]][addr_index_map[addr2]]
 
 
 def get_swap_difference(tour, v1, v2):
@@ -74,7 +23,6 @@ def swapped_edges(tour, v1, v2):
     new_tour.extend(tour[v2 + 1:])
     return new_tour
         
-
 
 def two_opt_tour(packages):
     tour = [HUB_ADDRESS]
@@ -103,17 +51,21 @@ def two_opt_tour(packages):
 
 
 if __name__ == '__main__':
-    package_map = OpenAddressHashTable()  # Maps package id -> Package object
-    address_index_map = {}  # Maps address string -> index in distance table
-    distance_table = []
+    distance_table = distance_table = data_loaders.load_distance_data("WGUPS Distance Table.csv")
+    package_map = data_loaders.load_package_data("WGUPS Package Table.csv")
+    addr_index_map = data_loaders.load_address_data("WGUPS Address Table.csv")
 
-    load_package_data()
-    load_address_data()
-    load_distance_data()
+    packages1 = []
+    packages2 = []
+    for i in range(1, 11):
+        packages1.append(package_map[i])
+    for i in range(31, 41):
+        packages1.append(package_map[i])
+    for i in range(21, 31):
+        packages2.append(package_map[i])
+    for i in range(11, 21):
+        packages2.append(package_map[i])
 
-    packages = []
-    for i in range(1, 40):
-        packages.append(package_map[i])
-
-    two_opt_tour(packages)
+    two_opt_tour(packages1)
+    two_opt_tour(packages2)
 
